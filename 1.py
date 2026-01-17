6933858e3451f7881a3be32428c9bd88c901f5fb
gassy-y <3553460176@qq.com>
2026-01-17 21:01:24 +0800

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-print(111)
-print(111)
-print('www','iii','sss',sep=',')    #sep=' '表示分隔的符号，写在最后
-# ctrl+/是添加注释，取消注释
-# ctrl+z是撤销上一步操作
-# ctrl+f是关键词查找
-# ctrl+d是复制到下一行
-print(33,end=',')    #end=' '表示以什么结尾，默认是换行
-print(11)
